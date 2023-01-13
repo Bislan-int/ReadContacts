097176350733a7a2b45e13b4eff0ed400f789109
Bislan <mamaliev.bislan@mail.ru>
2023-01-13 11:07:03 +0300

    Получение разрешения от пользователя

--- app/src/main/java/com/example/readcontacts/MainActivity.kt
+++ app/src/main/java/com/example/readcontacts/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.readcontacts
 
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlin.concurrent.thread
 
@@ -21,8 +21,32 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("MainActivity", "Permission denied")
+            requestPermission()
         }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("MainActivity", "Permission Denied")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun requestContacts() {
@@ -46,5 +70,10 @@ class MainActivity : AppCompatActivity() {
             }
             cursor?.close()
         }
+    }
+
+    companion object {
+
+        private const val READ_CONTACTS_RC = 100
     }
 }
